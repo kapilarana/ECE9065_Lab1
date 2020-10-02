8ba5578c8d224c3ff0f4ba2cde1f17ed12f7789a
kapilarana <67554582+kapilarana@users.noreply.github.com>
2020-10-01 19:39:04 -0700

    Dynamically create the 'div' to display search results. Clear out the last input char in search box if an integer value is more than 20.

--- myScript.js
+++ myScript.js
@@ -3,13 +3,24 @@
 
 "use strict";
 
+function closeSearchResults()
+{
+    divSR = document.getElementById("divSearchResults");
+    if (divSR != null) {
+	divSR.remove();
+    }
+
+    window.reload();
+}
+
 function pFind(input) {
-    //	  var input, filter, ul, li, a, i, txtValue;
     var filter, ul, li, a, i, txtValue;
     var results = "";
-    var isInt = 0;
     var divSR; // Div that will contain the search results
+
+    var input = document.getElementById('pSearchStr');
     
+    //console.log("input=" + input.value);
     if (input.value == "") {
 	window.location.reload();
 	return;
@@ -19,10 +30,25 @@ function pFind(input) {
 	var num = Number(input.value);
 	if (num < 0 || num > 20) {
 	    alert("ERROR: Invalid search value " + num + ": expecting 1-20.");
+	    var str=input.value;
+	    input.value = str.substring(0,str.length-1);
 	    return;
 	}
     }
+    // Upper case all input characters
+    filter = input.value.toUpperCase();
+
+    // Find the unordered list
+    ul = document.getElementById("pUl");
+
     divSR = document.getElementById("divSearchResults");
+    if (divSR == null) {
+//	divSR = document.createElement('div');
+//	divSR.setAttribute("id", "divSearchResults");
+	ul.insertAdjacentHTML('beforebegin', '<div id="divSearchResults"></div>');
+	divSR = document.getElementById("divSearchResults");
+    }
+    
     divSR.style.display = "block";
     var ulSR = document.getElementById("ulSR");
     if (ulSR != null) {
@@ -33,10 +59,6 @@ function pFind(input) {
     ulSR.setAttribute("id", "ulSR");
     divSR.appendChild(ulSR);
     
-    //console.log("input=" + input.value);
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("pUl");
-    //	  //console.log("ul.length=" + ul.length);
     li = ul.getElementsByTagName("li");
     //console.log("li.length=" + li.length);
     for (i = 0; i < li.length; i++) {
@@ -66,45 +88,4 @@ function pFind(input) {
         }
 //	//console.log("txtValue=" + txtValue + ", display=" + li[i].style.display);
     }
-    //console.log(divSR);
-    
-//    //alert(results);
-//    searchResultsStr = new String(results);
-//    //alert(searchResultsStr);
-//    //console.log(searchResultsStr);
-//    
-//    var psr = document.getElementById("pSearchResults");
-//    psr.innerHTML = results;
-//    //console.log(psr.innerHTML);
-//
-//    var modal = document.getElementById("myModal");
-//    //console.log(modal);
-//    modal.style.display = "block";
-//        
-//    // Get the button that opens the modal
-//    var btn = document.getElementById("myBtn");
-//    //console.log(btn);
-//    
-//    // Get the <span> element that closes the modal
-//    var span = document.getElementsByClassName("close")[0];
-//
-//    // When the user clicks the button, open the modal 
-//    btn.onclick = function() {
-//	modal.style.display = "block";
-//	//console.log("in btn.onclick");
-//    }
-//
-//    // When the user clicks on <span> (x), close the modal
-//    span.onclick = function() {
-//	modal.style.display = "none";
-//	//console.log("in span.onclick");
-//    }
-//
-    // When the user clicks anywhere outside of the modal, close it
-//    window.onclick = function(event) {
-//	if (event.target == modal) {
-//	    modal.style.display = "none";
-//	    //console.log("in window.onclick");
-//	}
-//    }
 }
